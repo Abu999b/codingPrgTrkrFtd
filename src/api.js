364593ee045664402ed0028656161313635c389f
MOHAMMED ABUBAKAR SIDDIQ <143837684+Abu999b@users.noreply.github.com>
2026-02-09 12:38:56 +0530

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://codingprgtrkrbkd.onrender.com/api'
 });
 
 // Add token to all requests automatically
